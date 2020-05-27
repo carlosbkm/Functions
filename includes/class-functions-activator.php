856c05e5fa32a7cbcdd5117a46475e94ce3b467b
Carlos Garcia <carlos.garcia.mejias@accenture.com>
2020-05-27 21:31:06 +0100

    Boilerplate first commit

---
+++ includes/class-functions-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://github.com/carlosbkm
+ * @since      1.0.0
+ *
+ * @package    Functions
+ * @subpackage Functions/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Functions
+ * @subpackage Functions/includes
+ * @author     Carlos Garcia <carlos.garcia.mejias@gmail.com>
+ */
+class Functions_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-functions-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://github.com/carlosbkm
+ * @since      1.0.0
+ *
+ * @package    Functions
+ * @subpackage Functions/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Functions
+ * @subpackage Functions/includes
+ * @author     Carlos Garcia <carlos.garcia.mejias@gmail.com>
+ */
+class Functions_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
